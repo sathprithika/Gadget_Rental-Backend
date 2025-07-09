870ce64ecd0d9679e607c9da585ab17cde5a4bfb
S Sathprithika <124038347+sathprithika@users.noreply.github.com>
2025-07-09 21:55:20 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { Hono } from 'hono'
+// import { Hono } from 'hono'
 
 // import { cors } from 'hono/cors'
+
+import { serve } from 'hono/node-server'
+import { Hono } from 'hono'
+
 
 type Laptop = {
   id: number;
